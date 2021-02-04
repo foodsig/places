24606a8bc060c96513d60f1ae2f157f7b2ca38ed
Riki Mitchell <riki@iteration22.co.nz>
2021-02-05 09:24:12 +1300

    remove index from export

--- places.py
+++ places.py
@@ -2,4 +2,4 @@ from pandas.io.excel import ExcelWriter
 import pandas
 
 with ExcelWriter("places.xlsx") as ew:
-    pandas.read_csv("places.csv").to_excel(ew)
+    pandas.read_csv("places.csv").to_excel(ew, index=False)
