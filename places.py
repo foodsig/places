8a9a1922b6c2344b40dcceb0baf71a46e5e9aa74
Hyunjin Kim <kim.hyunjin.paul@icloud.com>
2021-02-05 14:26:07 +1300

    eww double quotes

--- places.py
+++ places.py
@@ -1,5 +1,5 @@
 from pandas.io.excel import ExcelWriter
 import pandas
 
-with ExcelWriter("places.xlsx") as ew:
-    pandas.read_csv("places.csv").to_excel(ew, index=False)
+with ExcelWriter('places.xlsx') as ew:
+    pandas.read_csv('places.csv').to_excel(ew, index=False)
